b7c411965a4f3c8436fd8225e41b12c59570b89a
keepYourHeart <only0612@163.com>
2020-12-18 10:08:44 +0800

    day05

---
+++ src/main/java/com/hw/demo/Test714.java
@@ -0,0 +1,56 @@
+package com.hw.demo;
+
+import java.util.ArrayList;
+
+import static java.lang.Math.max;
+
+/**
+ *
+ * 给定一个整数数组 prices，其中第 i 个元素代表了第 i 天的股票价格 ；非负整数 fee 代表了交易股票的手续费用。
+ *
+ * 你可以无限次地完成交易，但是你每笔交易都需要付手续费。如果你已经购买了一个股票，在卖出它之前你就不能再继续购买股票了。
+ *
+ * 返回获得利润的最大值。
+ *
+ * 注意：这里的一笔交易指买入持有并卖出股票的整个过程，每笔交易你只需要为支付一次手续费。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * Created by huwei on 2020/12/17.
+ */
+public class Test714 {
+    public static int maxProfit(int[] prices, int fee) {
+//        int[][] dp  = new int[prices.length][2];
+//        int n = prices.length;
+//        if(n<=1)  return 0;
+//        //dp数组初始化
+//        dp[0][0]=0;
+//        dp[0][1]=-fee-prices[0];
+//
+//        for(int i=1;i<n;i++)
+//        {
+//            dp[i][0]=max(dp[i-1][1]+prices[i],dp[i-1][0]);//当天不持有股票的状态可能来自昨天有今天卖出或者昨天也不持有
+//            dp[i][1]=max(dp[i-1][0]-prices[i]-fee,dp[i-1][1]);//当天持有股票的状态可能来自昨天没有今天买进或者昨天就持有了
+//        }
+//        return dp[n-1][0];
+        int n = prices.length;
+        int buy = prices[0] + fee;
+        int profit = 0;
+        for (int i = 1; i < n; ++i) {
+            if (prices[i] + fee < buy) {
+                buy = prices[i] + fee;
+            } else if (prices[i] > buy) {
+                profit += prices[i] - buy;
+                buy = prices[i];
+            }
+        }
+        return profit;
+    }
+
+    public static void main(String[] args) {
+        int i = maxProfit(new int[]{1, 3, 7, 5, 10, 3}, 3);
+
+        System.out.println(i);
+    }
+}
---
+++ src/main/java/com/hw/demo/Test986.java
@@ -0,0 +1,47 @@
+package com.hw.demo;
+
+import java.util.ArrayList;
+
+/**
+ * 给定两个由一些 闭区间 组成的列表，每个区间列表都是成对不相交的，并且已经排序。
+ *
+ * 返回这两个区间列表的交集。
+ * Created by huwei on 2020/12/16.
+ */
+public class Test986 {
+    public static int[][] intervalIntersection(int[][] A, int[][] B) {
+        ArrayList<int[]> ints = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+        while(i<A.length && j<B.length){
+            int[] ints1 = A[i];
+            int[] ints2 = B[j];
+            if (ints1[0]>ints2[1]) {
+                j++;
+            }else if (ints1[1]<ints2[0]){
+                i++;
+            }else{
+                int[] ints3 = new int[2];
+                ints3[0] = Math.max(ints1[0],ints2[0]);
+                ints3[1] = Math.min(ints1[1],ints2[1]);
+                ints.add(ints3);
+                if (ints1[1]<ints2[1]){
+                    i++;
+                }else{
+                    j++;
+                }
+            }
+        }
+        int[][] result = new int[ints.size()][];
+        for (int ii = 0;ii<ints.size();ii++){
+            result[ii] = ints.get(ii);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        intervalIntersection(new int[][]{{0,2},{5,10},{13,23},{24,25}},new int[][]{{1,5},{8,12},{15,24},{25,26}});
+        
+    }
+
+}
